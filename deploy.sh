1da3dc3fe4da7c23571a33f91002982e37fd9a3f
Björn Kimminich <bjoern.kimminich@gmx.de>
2015-10-21 18:07:43 +0200

    added leading /

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl --ftp-create-dirs -T index.html -u $FTP_USER:$FTP_PASSWORD ftp://ftp.strato.de/webappsec-nutshell
+/curl --ftp-create-dirs -T index.html -u $FTP_USER:$FTP_PASSWORD ftp://ftp.strato.de/webappsec-nutshell
